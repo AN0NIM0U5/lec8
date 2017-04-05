45a05d25118abf3ec4014931167ab57b984c725c
Paige Plander <paigeplan@berkeley.edu>
2017-04-04 18:13:52 -0700

    starter code

--- Programmatic Design Demo/SecondViewController.swift
+++ Programmatic Design Demo/SecondViewController.swift
@@ -43,45 +43,16 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = Constants.viewColor
         
-        // add target to the button (so it will call `goToMain` when pressed)
-        secondButton.addTarget(self, action: #selector(goToMain), for: .touchDown)
-    
-        // IMPORTANT: this adds our button to the view.
-        view.addSubview(secondButton)
+        // TODO: add target to the button (so it will call `goToMain` when pressed)
         
-        // set up the constraints for the button
-        setupConstraints()
+        // TODO: add button to the view
+        
+        // TODO: set up the constraints for the button
         
     }
     
-    // Creates layout constraints for the button (notice how we never
-    // create a frame for the button, like we did in the MainViewController
-    func setupConstraints() {
-        let btn = secondButton
-        // Tell Xcode not to create its own constraints
-        secondButton.translatesAutoresizingMaskIntoConstraints = false
-        // center the button horizontally in the view
-        secondButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        // center the button vertically in the view
-        secondButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        // add a margin of `buttonMargin` (defined at top of file in struct) between view and left of the button
-        secondButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.buttonMargin).isActive = true
-        // add a margin of `buttonMargin` to right of button.
-        // this should be negative since we want it to the right of
-        // the view's margin
-        secondButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.buttonMargin).isActive = true
-        // constrains the button's height to a set size
-        secondButton.heightAnchor.constraint(equalToConstant: Constants.buttonHeight).isActive = true
-    }
-
-    /// Presents the main view controller
-    ///
-    /// - Parameter sender: the button that was pressed (`button`)
-    @IBAction func goToMain(sender: UIButton) {
-        let mainView = MainViewController()
-        self.present(mainView, animated: true, completion: nil)
-    }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
